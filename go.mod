9161fe8e985061078ed502c1b11aa4c41802c18d
Wizacass <visak.pet0@gmail.com>
2021-09-09 19:29:31 +0300

    Add input validation

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module vytenis.nanoavionics/keyboard_measurer
 
 go 1.17
+
+require (
+	github.com/eiannone/keyboard v0.0.0-20200508000154-caf4b762e807 // indirect
+	golang.org/x/sys v0.0.0-20210906170528-6f6e22806c34 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+type sessionStats struct {
+	totalKeys       int
+	mostPopularKeys [3]byte
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	intervalDuration, sessionDuration := validateArguments(os.Args[1:])
+
+	fmt.Println("Interval duration:", intervalDuration, "seconds.")
+	fmt.Println("Session duration:", sessionDuration, "seconds.")
+
+	controlChannel := make(chan int)
+
+	go startTimer(sessionDuration, controlChannel)
+
+	<-controlChannel
+	fmt.Println("Done!")
+}
+
+func validateArguments(args []string) (int, int) {
+	count := len(args)
+	if count != 2 {
+		message := fmt.Sprintf("\nThis program expects 2 arguments! You have provided %d", count)
+		panic(message)
+	}
+
+	intervalDuration := atoi(args[0])
+	sessionDuration := atoi(args[1])
+
+	if intervalDuration > sessionDuration {
+		panic("Interval duration cannot be longer than session duration!")
+	}
+
+	return intervalDuration, sessionDuration
+}
+
+func atoi(value string) int {
+	number, err := strconv.Atoi(value)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return number
+}
+
+func startTimer(seconds int, timeChannel chan int) {
+	timer := time.NewTimer(time.Duration(seconds * int(time.Second)))
+
+	go func() {
+		<-timer.C
+		timeChannel <- 1
+	}()
 }
